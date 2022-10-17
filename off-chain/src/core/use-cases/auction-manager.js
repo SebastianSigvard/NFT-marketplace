8c4729753ac84a3c151dd788f8ea95b918e04da6
SebastianSigvard <sebastiansigvard@gmail.com>
2022-10-17 09:52:37 -0300

    AuctionManager unit testing

--- off-chain/src/core/use-cases/auction-manager.js
+++ off-chain/src/core/use-cases/auction-manager.js
@@ -2,14 +2,14 @@ import auctionStorageInterface, {AuctionStorageInterface} from '../../infra/auct
 import TokenFactory from '../entities/token.js';
 import BidFactory from '../entities/bid.js';
 
-class AuctionManager {
+export default class AuctionManager {
   constructor(validator, auctionStorage = auctionStorageInterface) {
     if (! (Object.getPrototypeOf(auctionStorage) instanceof AuctionStorageInterface)) {
       throw Error('auctionStorage dep inject must be an instance of AuctionStorageInterface');
     }
 
     this.#auctionStorage = auctionStorage;
-    this.#tokenFactory = new TokenFactory();
+    this.#tokenFactory = new TokenFactory(validator);
     this.#bidFactory = new BidFactory(validator);
   }
 
@@ -22,7 +22,7 @@ class AuctionManager {
     const tokens = [];
 
     for (const token of _tokens) {
-      if (!token.tokenId || ! token.minPrice) {
+      if (token.tokenId === undefined || token.minPrice === undefined) {
         throw Error('All tokens must has tokenId and minPrice');
       }
 
@@ -46,13 +46,13 @@ class AuctionManager {
 
   // @param {Token} _token object {tokenId, minPrice}.
   addToken(listId, _token) {
-    if (!_token.tokenId || ! _token.minPrice) {
+    if (_token.tokenId === undefined || _token.minPrice === undefined) {
       throw Error('All tokens must has tokenId and minPrice');
     }
 
     const list = this.#auctionStorage.getList(listId);
 
-    const token = this.#tokenFactory.createToken(_token.listId, _token.minPrice);
+    const token = this.#tokenFactory.createToken(_token.tokenId, _token.minPrice);
 
     list.addToken(token);
 
@@ -106,13 +106,13 @@ class AuctionManager {
 
     token.addBid(bid);
 
-    this.#auctionStorage(list);
+    this.#auctionStorage.updateList(list);
   }
 
   deleteBid(listId, tokenId, bidderAddr) {
     const list = this.#auctionStorage.getList(listId);
 
-    const token = list.getToken(listId);
+    const token = list.getToken(tokenId);
 
     token.deleteBid(bidderAddr);
 
@@ -124,7 +124,7 @@ class AuctionManager {
 
     const token = list.getToken(tokenId);
 
-    return token.getBid();
+    return token.getBid(bidderAddr);
   }
 
   approveBid(listId, tokenId, bidderAddr, ownerSignature) {
@@ -143,6 +143,4 @@ class AuctionManager {
   #tokenFactory;
   #bidFactory;
 }
-
-export default new AuctionManager();
 
---
+++ off-chain/test/use-cases/auction-manager.test.js
@@ -0,0 +1,196 @@
+/* eslint-disable no-unused-vars */
+import AuctionManager from '../../src/core/use-cases/auction-manager.js';
+import MemAuctionStorage from '../../src/infra/memAuctionStorage.js';
+import ValidatorMock from '../validator_mock.js';
+
+const validatorMock = new ValidatorMock();
+const memAuctionStorage = new MemAuctionStorage(validatorMock);
+const auctionManager = new AuctionManager(validatorMock, memAuctionStorage);
+
+const minPrice = 10;
+const ownerAddr = 'fakeAddr';
+const bidderAddr = 'fakeAddr';
+const nftContractAddr = 'fakeAddr';
+const tokenId = 0;
+const erc20ContractAddr = 'fakeAddr';
+const erc20amount = 11;
+const bidderSignature = 'fakeBidderSignature';
+const ownerSignature = 'fakeOwnerSignature';
+
+test('AuctionManager list', async () => {
+  const list = auctionManager.createList(ownerAddr,
+      nftContractAddr,
+      [{tokenId, minPrice}],
+  );
+
+  expect(list.getListId()).toBe(0);
+  expect(list.getOwnerAddr()).toBe(ownerAddr);
+  expect(list.getNftContractAddr()).toBe(nftContractAddr);
+
+  const token = list.getToken(tokenId);
+
+  expect(token.getId()).toBe(tokenId);
+  expect(token.getMinPrice()).toBe(minPrice);
+  expect(token.getBids()).toStrictEqual([]);
+
+  auctionManager.deleteList(list.getListId());
+
+  const lists = auctionManager.getLists();
+
+  expect(lists.length).toBe(0);
+});
+
+test('AuctionManager token', async () => {
+  const list = auctionManager.createList(ownerAddr,
+      nftContractAddr,
+      [{tokenId, minPrice}],
+  );
+
+  auctionManager.addToken(list.getListId(),
+      {
+        tokenId: tokenId + 1,
+        minPrice: minPrice + 1,
+      },
+  );
+
+  let storedList = auctionManager.getList(list.getListId());
+  let tokens = storedList.getTokens();
+
+  expect(tokens.length).toBe(2);
+  expect(tokens[0].getId()).toBe(tokenId);
+  expect(tokens[0].getMinPrice()).toBe(minPrice);
+  expect(tokens[1].getId()).toBe(tokenId + 1);
+  expect(tokens[1].getMinPrice()).toBe(minPrice + 1);
+
+  auctionManager.deleteToken(list.getListId(), tokenId + 1);
+
+  storedList = auctionManager.getList(list.getListId());
+  tokens = storedList.getTokens();
+
+  expect(tokens.length).toBe(1);
+  expect(tokens[0].getId()).toBe(tokenId);
+  expect(tokens[0].getMinPrice()).toBe(minPrice);
+
+  const token0 = auctionManager.getToken(list.getListId(), tokenId);
+
+  expect(token0.getId()).toBe(tokenId);
+  expect(token0.getMinPrice()).toBe(minPrice);
+
+  auctionManager.deleteList(list.getListId());
+  const lists = auctionManager.getLists();
+  expect(lists.length).toBe(0);
+});
+
+test('AuctionManager bid', async () => {
+  const list = auctionManager.createList(ownerAddr,
+      nftContractAddr,
+      [{tokenId, minPrice}],
+  );
+
+  auctionManager.makeBid(ownerAddr,
+      bidderAddr,
+      nftContractAddr,
+      tokenId,
+      erc20ContractAddr,
+      erc20amount,
+      bidderSignature,
+  );
+
+  let bid = auctionManager.getBid(list.getListId(), tokenId, bidderAddr);
+
+  expect(bid.getOwnerAddr()).toBe(ownerAddr);
+  expect(bid.getBidderAddr()).toBe(bidderAddr);
+  expect(bid.getNftContractAddr()).toBe(nftContractAddr);
+  expect(bid.getTokenId()).toBe(tokenId);
+  expect(bid.getErc20ContractAddr()).toBe(erc20ContractAddr);
+  expect(bid.getErc20Amount()).toBe(erc20amount);
+  expect(bid.getOwnerSignature()).toBe(null);
+  expect(bid.getBidderSignature()).toBe(bidderSignature);
+
+  auctionManager.approveBid(list.getListId(),
+      tokenId,
+      bidderAddr,
+      ownerSignature,
+  );
+
+  bid = auctionManager.getBid(list.getListId(), tokenId, bidderAddr);
+
+  expect(bid.getOwnerAddr()).toBe(ownerAddr);
+  expect(bid.getBidderAddr()).toBe(bidderAddr);
+  expect(bid.getNftContractAddr()).toBe(nftContractAddr);
+  expect(bid.getTokenId()).toBe(tokenId);
+  expect(bid.getErc20ContractAddr()).toBe(erc20ContractAddr);
+  expect(bid.getErc20Amount()).toBe(erc20amount);
+  expect(bid.getOwnerSignature()).toBe(ownerSignature);
+  expect(bid.getBidderSignature()).toBe(bidderSignature);
+
+  auctionManager.deleteBid(list.getListId(), tokenId, bidderAddr);
+
+  const token = auctionManager.getToken(list.getListId(), tokenId);
+  const bids = token.getBids();
+
+  expect(bids.length).toBe(0);
+
+  auctionManager.deleteList(list.getListId());
+  const lists = auctionManager.getLists();
+  expect(lists.length).toBe(0);
+});
+
+
+test('AuctionManager throw test', async () => {
+  try {
+    const list = auctionManager.createList(ownerAddr,
+        nftContractAddr,
+        {tokenId, minPrice},
+    );
+
+    expect(true).toBe(false);
+  } catch (err) {
+    expect(err.message).toBe('tokens must be a non empty array');
+  }
+
+  try {
+    const list = auctionManager.createList(ownerAddr,
+        nftContractAddr,
+        [],
+    );
+
+    expect(true).toBe(false);
+  } catch (err) {
+    expect(err.message).toBe('tokens must be a non empty array');
+  }
+
+  try {
+    const list = auctionManager.createList(ownerAddr,
+        nftContractAddr,
+        [{}],
+    );
+
+    expect(true).toBe(false);
+  } catch (err) {
+    expect(err.message).toBe('All tokens must has tokenId and minPrice');
+  }
+
+  try {
+    auctionManager.addToken(0, {});
+    expect(true).toBe(false);
+  } catch (err) {
+    expect(err.message).toBe('All tokens must has tokenId and minPrice');
+  }
+
+  try {
+    auctionManager.makeBid(ownerAddr,
+        bidderAddr,
+        nftContractAddr,
+        tokenId,
+        erc20ContractAddr,
+        erc20amount,
+        bidderSignature,
+    );
+    expect(true).toBe(false);
+  } catch (err) {
+    expect(err.message).toBe('auctionList with ownerAddr ' + ownerAddr +
+        ' and nftContractAddr ' + nftContractAddr + ' not found');
+  }
+});
+
