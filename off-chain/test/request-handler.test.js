5fda5bad166f5f2c3d54a0dcad9ab398e3eaa5c4
SebastianSigvard <sebastiansigvard@gmail.com>
2022-10-19 20:05:06 -0300

    request handler test wip

--- off-chain/test/request-handler.test.js
+++ off-chain/test/request-handler.test.js
@@ -1,5 +1,7 @@
 import requestHandler from '../src/request-handler.js';
 import Web3 from 'web3';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const web3 = new Web3(process.env.API_URL);
 
@@ -9,6 +11,9 @@ const ownerAccount = web3.eth.accounts.privateKeyToAccount(
 const bidderAccount = web3.eth.accounts.privateKeyToAccount(
   process.env.TEST_BIDDER_PRIVATE_KEY
 );
+
+web3.eth.accounts.wallet.add(ownerAccount);
+web3.eth.accounts.wallet.add(bidderAccount);
 
 const minPrice = 10;
 const ownerAddr = ownerAccount.address;
@@ -20,13 +25,17 @@ const erc20amount = 11;
 
 test('RequestHandler list', async () => {
   const message = web3.utils.soliditySha3(ownerAddr, nftContractAddr);
-  const ownerSignature = await web3.eth.accounts.sign(message, process.env.TEST_OWNER_PRIVATE_KEY);
+  const ownerSignature = await web3.eth.sign(message, ownerAddr);
 
-  const res = requestHandler.createList({
+  const res = await requestHandler.createList({
     ownerAddr,
     nftContractAddr,
     tokens: [{tokenId, minPrice}],
     ownerSignature
   });
+
+  console.log(res.message);
+  expect(res.status).toBe(true);
+  expect(res.message).toBe('gg');
 });
 
